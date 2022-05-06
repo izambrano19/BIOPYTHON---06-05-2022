6a855366abe84ffbf160c3a9ec9367f18dfd0d3c
super <izambrano19@ilg.cat>
2022-05-06 17:32:51 +0200

    PISTAS PARA LA PRACTICA - 06-05-2022

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ Entrez.email = "izambrano19@ilg.cat"  # Always tell NCBI who you are
 #  NM_001382665.1
 
 with open("resultatexemple.txt", "w") as fitxer:
-    handle = Entrez.efetch(db="nucleotide", id="NM_001382665.1", rettype="gb", retmode="text")
+    handle = Entrez.efetch(db="nucleotide", id="NM_001354619.2", rettype="gb", retmode="text")
+    handlee = Entrez.efetch(db="nucleotide", id="NM_001324522.1", rettype="gb", retmode="text")
     for rec in SeqIO.parse(handle, "gb"):
+        j = 0
         fitxer.write(rec.id)
         fitxer.write("\n")
         fitxer.write(rec.description)
         fitxer.write("\n")
         #fitxer.write(str(rec.features))
-        fitxer.write("\n")
         ##una feature es d'aquesta manera
         ##SeqFeature(FeatureLocation(ExactPosition(0), ExactPosition(1884), strand=1), type='source')
         ##SeqFeature(FeatureLocation(ExactPosition(0), ExactPosition(1884), strand=1), type='gene')
         for i in (rec.features):
-            fitxer.write(i.type + "\n")
-            fitxer.write(str(i.location) + "\n")
-            fitxer.write(str(i.location.strand) + "\n")
-            fitxer.write(str(i.qualifiers.keys()) + "\n")
             if i.type == 'exon':
-                fitxer.write(str(i.qualifiers['gene']) + "\n")
+                j +=1
+                fitxer.write(i.type)
+                fitxer.write(str(" numero " + str(j) + " es:") + "\n")
+                fitxer.write(str("  " + str(i.location) + "\n"))
 
 
 
