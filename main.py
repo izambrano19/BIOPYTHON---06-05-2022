8919f7bc3d777671c5f386d53ce226ba30b649b4
super <izambrano19@ilg.cat>
2022-05-10 17:14:36 +0200

    COMMIT 10-05-2022

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Entrez.email = "izambrano19@ilg.cat"  # Always tell NCBI who you are
 
 
 with open("practica.txt", "w") as fitxer:
-    #  NM_001354619.2
-    handle = Entrez.efetch(db="nucleotide", id="NM_001354619.2", rettype="gb", retmode="text")
-    #  NM_001324522.1
-    handlee = Entrez.efetch(db="nucleotide", id="NM_001324522.1", rettype="gb", retmode="text")
+    #  NM_001354619.2     NM_001324522.1
+    handle = Entrez.efetch(db="nucleotide", id="NM_001354619.2 , NM_001324522.1", rettype="gb", retmode="text")
+
+
     for rec in SeqIO.parse(handle, "gb"):
         j = 0
         fitxer.write("\n")
@@ -23,31 +23,25 @@ with open("practica.txt", "w") as fitxer:
                 j +=1
                 fitxer.write(str("Exo numero " + str(j) + " es:") + "\n")
                 fitxer.write(str("  " + str(i.location) + "\n"))
+
         for i in (rec.features):
             if i.type == 'CDS':
                 fitxer.write("\n")
-                fitxer.write(str("El CDS te ") + "\n")
+                fitxer.write(str("El CDS te: ") + "\n")
                 fitxer.write(str("  " + str(i.location) + "\n"))
+                e = str(i.qualifiers['translation']).replace("[", "")
+                g = e.replace("]", "")
+                f = e.replace("'", "")
+                fitxer.write(f + "\n")
+                fitxer.write(str(len(f) * 3))
 
 
 
 
 
 
-    for rec in SeqIO.parse(handlee, "gb"):
-        j = 0
-        fitxer.write("\n")
-        fitxer.write(rec.description)
-        fitxer.write("\n")
-        for i in (rec.features):
-            if i.type == 'exon':
-                j +=1
-                fitxer.write(str("Exo numero " + str(j) + " es:") + "\n")
-                fitxer.write(str("  " + str(i.location) + "\n"))
-        for i in (rec.features):
-            if i.type == 'CDS':
-                fitxer.write("\n")
-                fitxer.write(str("El CDS te ") + "\n")
-                fitxer.write(str("  " + str(i.location) + "\n"))
 
 
+
+
+#Un aminoacido por ejemplo M son 3 nucleotidos AUG, multiplicamos la M por
