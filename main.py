524da3776cbc68848dc0cbccc53534b07f7908b7
super <izambrano19@ilg.cat>
2022-05-13 19:17:42 +0200

    TERMINADO

--- main.py
+++ main.py
@@ -6,20 +6,15 @@ from Bio import Seq
 
 Entrez.email = "izambrano19@ilg.cat"  # Always tell NCBI who you are
 
-c1 = []
-c2 = []
-vacio = []
-
+c1 = 0
+c2 = 0
 
 def alinea(cadena1, cadena2):
     valor = 0
     for i in range(len(cadena1)):
         if cadena1[i] == cadena2[i]:
             valor += 1
-        else:
-            valor -= 1
-            return valor
-
+    return valor
 
 with open("practica.txt", "w") as fitxer:
     #  NM_001354619.2     NM_001324522.1
@@ -39,7 +34,8 @@ with open("practica.txt", "w") as fitxer:
 
         for i in (rec.features):
             if i.type == 'CDS':
-                if c1 == vacio:
+                if c1 == 0:
+                    d1 = rec.description
                     fitxer.write("\n")
                     c1 = str(i.qualifiers['translation']).strip("[']")
                     fitxer.write(str("El CDS te: ") + str(len(c1) * 3) + "\n")
@@ -50,6 +46,7 @@ with open("practica.txt", "w") as fitxer:
                     fitxer.write(c1 + "\n")
 
                 else:
+                    d2 = rec.description
                     fitxer.write("\n")
                     c2 = str(i.qualifiers['translation']).strip("[']")
                     fitxer.write(str("El CDS te: ") + str(len(c2) * 3) + "\n")
@@ -58,4 +55,6 @@ with open("practica.txt", "w") as fitxer:
                     fitxer.write(dna + "\n")
                     c2 = c2[0:50]
                     fitxer.write(c2 + "\n")
-print(alinea(c1, c2))
+                    fitxer.write("\n")
+    fitxer.write("La igualtat entre " + d1 + " i " + d2 + " es " + str(alinea(c1,c2)) + " sobre 50")
+
