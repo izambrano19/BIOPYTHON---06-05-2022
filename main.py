c408a2e44ed2f8b204cd788381f441662694cdc6
super <izambrano19@ilg.cat>
2022-05-13 16:23:02 +0200

    TERMINADO - Ismael Zambrano

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from Bio import Entrez
 from Bio import SeqIO
+import dnachisel
+from dnachisel.biotools import reverse_translate
 from Bio import Seq
+
 Entrez.email = "izambrano19@ilg.cat"  # Always tell NCBI who you are
 
 
@@ -8,16 +11,12 @@ with open("practica.txt", "w") as fitxer:
     #  NM_001354619.2     NM_001324522.1
     handle = Entrez.efetch(db="nucleotide", id="NM_001354619.2 , NM_001324522.1", rettype="gb", retmode="text")
 
-
     for rec in SeqIO.parse(handle, "gb"):
         j = 0
         fitxer.write("\n")
         fitxer.write(rec.description)
         fitxer.write("\n")
-        #fitxer.write(str(rec.features))
-        ##una feature es d'aquesta manera
-        ##SeqFeature(FeatureLocation(ExactPosition(0), ExactPosition(1884), strand=1), type='source')
-        ##SeqFeature(FeatureLocation(ExactPosition(0), ExactPosition(1884), strand=1), type='gene')
+
         for i in (rec.features):
             if i.type == 'exon':
                 j +=1
@@ -29,22 +28,8 @@ with open("practica.txt", "w") as fitxer:
                 fitxer.write("\n")
                 e = str(i.qualifiers['translation']).strip("[']")
                 fitxer.write(str("El CDS te: ") + str(len(e) * 3) + "\n")
-                fitxer.write(str("  " + str(i.location) + "\n"))
+                fitxer.write(str(i.location) + "\n")
+                dna = reverse_translate(str(e))
+                fitxer.write(dna + "\n")
+                e = e[0:50]
                 fitxer.write(e + "\n")
-                ## e = str(i.qualifiers['translation']).replace("[", "")
-                ## g = e.replace("]", "")
-                ## f = g.replace("'", "")
-                ## fitxer.write(f + "\n")
-                ## fitxer.write(str(len(f) * 3) + "\n")
-                fitxer.write(e + "\n")
-
-
-
-
-
-
-
-
-
-
-#Un aminoacido por ejemplo M son 3 nucleotidos AUG, multiplicamos la M por
